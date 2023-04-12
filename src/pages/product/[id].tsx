f529f547ae42dfc4191959b228b8c6ffd651d69e
Marcos André <72817154+Marcos-afk@users.noreply.github.com>
2023-04-12 03:18:05 -0300

    :hammer: applying SSG with dynamic parameter

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,16 +1,32 @@
-import Explorer from '@assets/shirts/explorer.png';
+import { stripe } from '@lib/stripe';
 import * as S from '@styles/pages/product';
+import { GetStaticPaths, GetStaticProps } from 'next';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
+import Stripe from 'stripe';
 
-const Product = () => {
-  const { query } = useRouter();
+interface ProductProps {
+  product: {
+    id: number;
+    name: string;
+    description: string;
+    imageUrl: string;
+    price: string;
+  };
+}
+
+const Product = ({ product }: ProductProps) => {
+  const { isFallback } = useRouter();
+
+  if (isFallback) {
+    return <p>Carregando...</p>;
+  }
 
   return (
     <S.Container>
       <S.ImageContainer>
         <Image
-          src={Explorer}
+          src={product.imageUrl}
           alt="Explorer"
           width={520}
           height={480}
@@ -21,14 +37,9 @@ const Product = () => {
         />
       </S.ImageContainer>
       <S.ProductDetails>
-        <h1>Explorer</h1>
-        <span>R$ 79,90</span>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-          Reprehenderit placeat voluptates ducimus mollitia consequuntur iusto,
-          autem est adipisci asperiores laborum laudantium esse, ipsam ex
-          facilis aliquid praesentium nesciunt nobis vel?
-        </p>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
+        <p>{product.description}</p>
         <button>Comprar agora</button>
       </S.ProductDetails>
     </S.Container>
@@ -36,3 +47,50 @@ const Product = () => {
 };
 
 export default Product;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      {
+        params: {
+          id: 'prod_NhD9pveCRioPOT',
+        },
+      },
+    ],
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}) => {
+  if (!params) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const productId = params.id;
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price'],
+  });
+
+  const price = product.default_price as Stripe.Price;
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        description: product.description,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format((price.unit_amount as number) / 100),
+      },
+    },
+    revalidate: 60 * 60 * 24,
+  };
+};
